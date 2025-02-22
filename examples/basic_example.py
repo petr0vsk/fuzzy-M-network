ec96357c81046190b2619202f093cccdbede6e55
petr0vsk <petr0vskjy.aleksander@gmail.com>
2025-02-22 16:35:56 +0300

    Добавили приведение типа в методе класса

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -1,12 +1,18 @@
 # examples/basic_example.py
 
-from m_network.neuron import Neuron  # Импортируем класс
+from m_network.neuron import Neuron
 
-# Создаем объект нейрона
+# Создаем нейрон
 n1 = Neuron(neuron_id=1)
-n2 = Neuron(neuron_id=2, initial_value=0.75)
-print(type(n1))  # Проверим тип объекта
-# Вывод информации о нейронах
-n1.display_info()  # Ожидаемый вывод: Neuron ID: 1, Value: 0.0
-n2.display_info()  # Ожидаемый вывод: Neuron ID: 2, Value: 0.75
-print(Neuron.__doc__)
+
+# Выводим начальную информацию
+n1.display_info()  # Neuron ID: 1, Value: 0.0
+
+# Меняем значение нейрона
+n1.set_value(0.85)
+
+# Проверяем новое значение
+print("Новое значение нейрона:", n1.get_value())  # Новое значение нейрона: 0.85
+
+# Выводим обновленную информацию
+n1.display_info()  # Neuron ID: 1, Value: 0.85
--- m_network/neuron.py
+++ m_network/neuron.py
@@ -1,13 +1,27 @@
 class Neuron:
+    """
+       Класс для представления нейрона в М-сети.
+    """
     def __init__(self, neuron_id, initial_value=0.0):
         """
-              Конструктор для инициализации нейрона.
-
-              :param neuron_id: Уникальный идентификатор нейрона.
-              :param initial_value: Начальное значение нейрона (по умолчанию 0.0).
+        Конструктор для инициализации нейрона.
+        :param neuron_id: Уникальный идентификатор нейрона.
+        :param initial_value: Начальное значение нейрона (по умолчанию 0.0).
         """
         self.neuron_id = neuron_id
         self.value = initial_value
     def display_info(self):
         """печатает информацию о нейроне"""
         print(f"Neuron_id: {self.neuron_id}, Value: {self.value}")
+    def set_value(self, new_value: float) -> None:
+        """
+        Устанавливает новое значение для нейрона.
+        :param new_value: Новое значение для нейрона.
+        """
+        self.value = new_value
+   def get_value(self) -> float:
+       """
+        Возвращает текущее значение нейрона.
+        :return: Значение нейрона.
+       """
+       return self.value
