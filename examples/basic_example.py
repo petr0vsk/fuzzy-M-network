9285ac9695788390e5a877876d0ab2431317fc9b
petr0vsk <petr0vskjy.aleksander@gmail.com>
2025-02-22 16:19:41 +0300

    Добавили чтение докстринг

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -9,3 +9,4 @@ print(type(n1))  # Проверим тип объекта
 # Вывод информации о нейронах
 n1.display_info()  # Ожидаемый вывод: Neuron ID: 1, Value: 0.0
 n2.display_info()  # Ожидаемый вывод: Neuron ID: 2, Value: 0.75
+print(Neuron.__doc__)
